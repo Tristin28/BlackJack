2b556d644525ed1625e9948a1150e0b35b575922
Tristin28 <tristin.bezzina.24@um.edu.mt>
2026-04-02 18:17:46 +0200

    slight changes

--- DoubleQLearningAgent.py
+++ DoubleQLearningAgent.py
@@ -4,3 +4,9 @@ class DoubleQLearningAgent():
     
     def update_q_value(self):
         pass
+
+    def choose_action(self,state,epsilon):
+        '''
+            As it uses both Q-tables to select the action with the highest value, and then updates the Q-table that was not used for action selection
+        '''
+        pass
