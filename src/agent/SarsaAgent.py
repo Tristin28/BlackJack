9d32d7a603305e5d846547991e155345da9f5017
Tristin28 <tristin.bezzina.24@um.edu.mt>
2026-04-03 17:23:10 +0200

    I need a function from env - look at comment

--- src/agent/SarsaAgent.py
+++ src/agent/SarsaAgent.py
@@ -7,5 +7,5 @@ class SarsaAgent(BaseAgent):
         self.q_table[state][action] += self.get_alpha() * (reward + self.gamma * self.q_table[next_state][next_action] - self.q_table[state][action])
 
     def training_loop(self, environment_instance,epsilon):
-        #Need some sort of function which gives me a begining state from environment instance, i.e. state = environment_instance.get_initial_state() or something like that
+        #Need some sort of function which gives me a begining state from environment instance i.e. state = environment_instance.get_initial_state() or something like that
         pass
